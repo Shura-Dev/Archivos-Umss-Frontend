70f13097ce213e849d32f4464a0d1bf3c963b45e
pattyt0 <pattyopatito@gmail.com>
2022-02-22 15:37:40 -0400

    - listar las configuraciones de busqueda

--- src/app/modules/servicios/servicios.module.ts
+++ src/app/modules/servicios/servicios.module.ts
@@ -1,22 +1,28 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {ConfiguracionesComponent} from './configuraciones/configuraciones.component';
 import {TramitesComponent} from './tramites/tramites.component';
 import {ReportesComponent} from './reportes/reportes.component';
 import {ServiciosComponent} from './servicios.component';
 import {ServiciosRoutingModule} from "./servicios-routing.module";
+import {InlineSVGModule} from "ng-inline-svg";
+import { CrearConfiguracionesComponent } from './configuraciones/components/crear-configuraciones.component';
+import {ListaConfiguracionesComponent} from "./configuraciones/lista-configuraciones.component";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    ConfiguracionesComponent,
+    ListaConfiguracionesComponent,
     TramitesComponent,
     ReportesComponent,
-    ServiciosComponent
+    ServiciosComponent,
+    CrearConfiguracionesComponent
   ],
-  imports: [
-    CommonModule,
-    ServiciosRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        ServiciosRoutingModule,
+        InlineSVGModule,
+        ReactiveFormsModule
+    ]
 })
 export class ServiciosModule {
 }
